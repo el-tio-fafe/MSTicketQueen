32cbdc091e7917ddc452f8a2d5ad97c9b296c093
cote-huaiqui <ma.crucesh@duocuc.cl>
2026-05-08 20:13:30 -0400

    cree msComprador
    este es el micro serv del comprador que es el cliente final que compra los tickets

---
+++ msComprador/src/main/java/cl/duoc/msComprador/MsCompradorApplication.java
@@ -0,0 +1,13 @@
+package cl.duoc.msComprador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsCompradorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsCompradorApplication.class, args);
+	}
+
+}
---
+++ msComprador/src/test/java/cl/duoc/msComprador/MsCompradorApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duoc.msComprador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsCompradorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
