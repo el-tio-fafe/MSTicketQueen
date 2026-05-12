af3727fb25a2b14fb0c7c960d8ba6060f62e758d
cote-huaiqui <ma.crucesh@duocuc.cl>
2026-05-12 12:51:54 -0400

    arregle el Data Loader de Facturacion
    agregue 3 bancos, pero falta agregar formas de pago y comprobantes
    @Cris

--- MSFacturacion/src/main/java/cl/duoc/MSFacturacion/Config/Dataloader.java
+++ MSFacturacion/src/main/java/cl/duoc/MSFacturacion/Config/Dataloader.java
@@ -1,22 +1,39 @@
 package cl.duoc.MSFacturacion.Config;
 
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import cl.duoc.MSFacturacion.model.banco;
-import cl.duoc.MSFacturacion.model.formaPago;
 import cl.duoc.MSFacturacion.repository.ComprobanteRepository;
 import cl.duoc.MSFacturacion.repository.FormaDePagoRepository;
 import cl.duoc.MSFacturacion.repository.bancoRepository;
 
 @Configuration
 public class Dataloader {
-    CommandLineRunner initDataBase(bancoRepository bancoRepo , ComprobanteRepository comprobanteRepo , FormaDePagoRepository formaDePagoRepo ){
-   return arg -> {
-banco banco1 =
-bancoRepo.save(banco1);
 
-   }
+   @Bean
+   CommandLineRunner initDataBase(bancoRepository bancoRepo , ComprobanteRepository comprobanteRepo , FormaDePagoRepository formaDePagoRepo ){
+   return arg -> {
+         if(bancoRepo.count() > 0){
+
+            System.out.println("No se cargaron datos porque ya existian datos en la base de datos");
+
+         }else{
+
+            banco banco1 = new banco(null, "Estado");
+            banco banco2 = new banco(null, "Santander");
+            banco banco3 = new banco(null, "Falabella");
+
+            bancoRepo.save(banco1);
+            bancoRepo.save(banco2);
+            bancoRepo.save(banco3);
+
+
+
+         }
+
+   };
 }
     
 
