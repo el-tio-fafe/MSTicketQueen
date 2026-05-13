b66a922376c372dcd95efdaa6d254165ff55fad8
cote-huaiqui <ma.crucesh@duocuc.cl>
2026-05-13 19:06:57 -0400

    .

---
+++ msVentaTicket/src/main/java/cl/duoc/msVentaTicket/model/Ticket.java
@@ -0,0 +1,18 @@
+package cl.duoc.msVentaTicket.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "ticket")
+
+
+public class Ticket {
+
+}
