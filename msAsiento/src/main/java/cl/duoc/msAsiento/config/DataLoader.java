1994a15951483ae9a5aa1afdb8ec0c02f16cdb68
cote-huaiqui <ma.crucesh@duocuc.cl>
2026-05-13 11:55:37 -0400

    mis 4 ms estan con el Data Loader listo
    ya estan terminados falta conectarlos entre si

--- msAsiento/src/main/java/cl/duoc/msAsiento/config/DataLoader.java
+++ msAsiento/src/main/java/cl/duoc/msAsiento/config/DataLoader.java
@@ -4,27 +4,37 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import cl.duoc.msAsiento.model.Asiento;
+import cl.duoc.msAsiento.repository.AsientoRepository;
+import cl.duoc.msAsiento.repository.ReservaTemporalRepository;
+
 @Configuration
 public class DataLoader {
 
     @Bean                        //LLAMAMOS A TODOS LOS REPOSITORIOS QUE TENEMOS CON SUS OBJETOS (dentro del())
-    CommandLineRunner initDataBase(  ){
+    CommandLineRunner initDataBase(AsientoRepository asientoRepository, ReservaTemporalRepository reservaTemporalRepository){
         return args -> {
 
-            //SI(EL OBJETO DEL repositorio PPAL ES >0)
-            if(    > 0){
+            //SI(EL OBJETO DEL repositorio PPAL.count() ES >0)
+            if( asientoRepository.count() > 0){
                 System.out.println("No se cargaron datos porque ya existian datos en la base de datos");
             }else{
 
                 //PRIMERO PONER NOMBRES DE NUEVOS ATRIBUTOS PARA LLENAR LAS TABLAS ASI:
-                //NOMBRE DE CADA CLASE1 DEL MODEL clase1 = new CLASE1 (atributos);
-
+                //NOMBRE DE CADA CLASE1 DEL MODEL clase1 = new CLASE1 (atributos);   //ESTADO ASIENTO: DISPONIBLE, RESERVADO O VENDIDO
+                Asiento asien1 = new Asiento(null, "A100", "VENDIDO");
+                Asiento asien2 = new Asiento(null, "B200", "DISPONIBLE");
+                Asiento asien3 = new Asiento(null, "C300", "DISPONIBLE");
+                Asiento asien4 = new Asiento(null, "D400", "VENDIDO");
+                Asiento asien5 = new Asiento(null, "E500", "DISPONIBLE");
 
 
                 //clase1Repo.save(clase1)
-
-
-
+                asientoRepository.save(asien1);
+                asientoRepository.save(asien2);
+                asientoRepository.save(asien3);
+                asientoRepository.save(asien4);
+                asientoRepository.save(asien5);
 
 
                 System.out.println("Datos cargados con exito a la base de datos");
--- msComprador/src/main/java/cl/duoc/msComprador/config/DataLoader.java
+++ msComprador/src/main/java/cl/duoc/msComprador/config/DataLoader.java
@@ -4,25 +4,33 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import cl.duoc.msComprador.model.Comprador;
+import cl.duoc.msComprador.repository.CompradorRepository;
+
 @Configuration
 public class DataLoader {
 
     @Bean                        //LLAMAMOS A TODOS LOS REPOSITORIOS QUE TENEMOS CON SUS OBJETOS (dentro del())
-    CommandLineRunner initDataBase(  ){
+    CommandLineRunner initDataBase(CompradorRepository compradorRepository){
         return args -> {
 
             //SI(EL OBJETO DEL repositorio PPAL.count() ES >0)
-            if(    > 0){
+            if(compradorRepository.count()   > 0){
                 System.out.println("No se cargaron datos porque ya existian datos en la base de datos");
             }else{
 
                 //PRIMERO PONER NOMBRES DE NUEVOS ATRIBUTOS PARA LLENAR LAS TABLAS ASI:
                 //NOMBRE DE CADA CLASE1 DEL MODEL clase1 = new CLASE1 (atributos);
-
-
+                Comprador comp1 = new Comprador(null, "23388916-4", "Matias", "Gomez", "Cruces", "mati@gmail.com", "+569 84578596", "cuaderno");
+                Comprador comp2 = new Comprador(null, "25008098-0", "Abraham", "Cortes", "Perez", "aby@gmail.com", "+569 52584565", "moto");
+                Comprador comp3 = new Comprador(null, "9989533-0", "Rosa", "Huaiquil", "Briones", "rosa@gmail.com", "+569 93944430", "bebida70");
+                Comprador comp4 = new Comprador(null, "17122488-2", "Alejandro", "Cruces", "Alcantara", "ale@gmail.com", "+569 22233565", "alejo");
 
                 //clase1Repo.save(clase1)
-
+                compradorRepository.save(comp1);
+                compradorRepository.save(comp2);
+                compradorRepository.save(comp3);
+                compradorRepository.save(comp4);
 
 
 
