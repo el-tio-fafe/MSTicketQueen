5732f1766d9a3a370dd478182fa611f493256f34
cote-huaiqui <ma.crucesh@duocuc.cl>
2026-05-13 15:12:43 -0400

    creacion model y TipoUsuario

--- msLogin/src/main/java/cl/duoc/msLogin/model/TipoUsuario.java
+++ msLogin/src/main/java/cl/duoc/msLogin/model/TipoUsuario.java
@@ -1,3 +1,34 @@
+package cl.duoc.msLogin.model;
+
+
+import jakarta.persistence.*;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "tipo_usuario")
 public class TipoUsuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+
+    @Column(nullable = false)
+    private String nombreTipoUsuario;
+
+
+    @OneToOne
+    private Usuario usuario;
+
+    
+
+
+
 
 }
