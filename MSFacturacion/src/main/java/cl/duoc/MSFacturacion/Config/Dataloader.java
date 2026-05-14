47f8985b722661ced12534a09c01d0fd6bb857d7
tobalvaldes <cr.valdesc@duocuc.cl>
2026-05-14 12:36:29 -0400

    dataloader facturacion

--- MSFacturacion/src/main/java/cl/duoc/MSFacturacion/Config/Dataloader.java
+++ MSFacturacion/src/main/java/cl/duoc/MSFacturacion/Config/Dataloader.java
@@ -4,7 +4,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import cl.duoc.MSFacturacion.model.Comprobante;
 import cl.duoc.MSFacturacion.model.banco;
+import cl.duoc.MSFacturacion.model.formaPago;
 import cl.duoc.MSFacturacion.repository.ComprobanteRepository;
 import cl.duoc.MSFacturacion.repository.FormaDePagoRepository;
 import cl.duoc.MSFacturacion.repository.bancoRepository;
@@ -30,12 +32,19 @@ public class Dataloader {
             bancoRepo.save(banco3);
 
             //CARGAR DATOS DE COMPROBANTE
-
-
+            Comprobante comprobante1 = new Comprobante(null, "123456", null, 20000, null, false, null, banco3);
+            Comprobante comprobante2 = new Comprobante(null, "654321", null, 10000, null, false, null, banco2);
+            Comprobante comprobante3 = new Comprobante(null, "789012", null, 30000, null, false, null, banco1);
+            comprobanteRepo.save(comprobante1);
+            comprobanteRepo.save(comprobante2);
+            comprobanteRepo.save(comprobante3);
             //CARGAR DATOS DE FORMA DE PAGO
-
-
-
+            formaPago forma1 = new formaPago(null, "Efectivo", null);
+            formaPago forma2 = new formaPago(null, "Tarjeta de credito", null);
+            formaPago forma3 = new formaPago(null, "Tarjeta Debito", null);
+            formaDePagoRepo.save(forma1);
+            formaDePagoRepo.save(forma2);
+            formaDePagoRepo.save(forma3);
             System.out.println("Datos cargados con exito a la base de datos");
          }
 
