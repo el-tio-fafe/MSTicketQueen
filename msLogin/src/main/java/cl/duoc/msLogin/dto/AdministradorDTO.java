6a5f035a7b8f9481877e73c29b8afbe4c233f03e
Benjamín Andurandegui <bi.valenzuela10@gmail.com>
2026-06-03 16:55:29 -0400

    mini arreglo y implementacion de dependencias
    arregle lo que faltaba en ms login y tambien implemente las dependencias que faltaban en todos los ms tambien cambie las "aplication.propierties" a "yml"

---
+++ msLogin/src/main/java/cl/duoc/msLogin/dto/AdministradorDTO.java
@@ -0,0 +1,19 @@
+package cl.duoc.msLogin.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class AdministradorDTO {
+
+
+    private Integer idAdm;
+    private String nombreAdm;
+    private String apPatAdm;
+    private String rutAdm;
+    private String correoAdm;
+}
---
+++ msLogin/src/main/java/cl/duoc/msLogin/dto/CompradorDTO.java
@@ -0,0 +1,19 @@
+package cl.duoc.msLogin.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class CompradorDTO {
+
+    private Integer idCliente;
+    private String rutCliente;
+    private String nombreCliente;
+    private String apPaternoCliente;
+    private String correoCliente;
+
+}
