6e23be679248a06cd5c3f794c11d43f1f565da27
Benjamín Andurandegui <bi.valenzuela10@gmail.com>
2026-06-08 12:24:21 -0400

    arreglo facturacion

--- MSFacturacion/src/main/java/cl/duoc/MSFacturacion/Config/Dataloader.java
+++ MSFacturacion/src/main/java/cl/duoc/MSFacturacion/Config/Dataloader.java
@@ -1,21 +1,22 @@
 package cl.duoc.MSFacturacion.Config;
 
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import cl.duoc.MSFacturacion.model.Comprobante;
-import cl.duoc.MSFacturacion.model.banco;
-import cl.duoc.MSFacturacion.model.formaPago;
+import cl.duoc.MSFacturacion.model.Banco;
+import cl.duoc.MSFacturacion.model.FormaPago;
 import cl.duoc.MSFacturacion.repository.ComprobanteRepository;
-import cl.duoc.MSFacturacion.repository.FormaDePagoRepository;
-import cl.duoc.MSFacturacion.repository.bancoRepository;
+import cl.duoc.MSFacturacion.repository.FormaPagoRepository;
+import cl.duoc.MSFacturacion.repository.BancoRepository;
 
 @Configuration
 public class Dataloader {
 
    @Bean
-   CommandLineRunner initDataBase(bancoRepository bancoRepo , ComprobanteRepository comprobanteRepo , FormaDePagoRepository formaDePagoRepo ){
+   CommandLineRunner initDataBase(BancoRepository bancoRepo , ComprobanteRepository comprobanteRepo , FormaPagoRepository formaDePagoRepo ){
    return arg -> {
          if(bancoRepo.count() > 0){
 
@@ -23,28 +24,56 @@ public class Dataloader {
 
          }else{
 
-            banco banco1 = new banco(null, "Estado", null);
-            banco banco2 = new banco(null, "Santander", null);
-            banco banco3 = new banco(null, "Falabella", null);
+            Banco banco1 = new Banco(null, "Estado", null);
+            Banco banco2 = new Banco(null, "Falabella", null);
+            Banco banco3 = new Banco(null, "Santander", null);
 
             bancoRepo.save(banco1);
             bancoRepo.save(banco2);
             bancoRepo.save(banco3);
 
-            //CARGAR DATOS DE COMPROBANTE
-            Comprobante comprobante1 = new Comprobante(null, "123456", null, 20000, null, false, null, banco3);
-            Comprobante comprobante2 = new Comprobante(null, "654321", null, 10000, null, false, null, banco2);
-            Comprobante comprobante3 = new Comprobante(null, "789012", null, 30000, null, false, null, banco1);
-            comprobanteRepo.save(comprobante1);
-            comprobanteRepo.save(comprobante2);
-            comprobanteRepo.save(comprobante3);
-            //CARGAR DATOS DE FORMA DE PAGO
-            formaPago forma1 = new formaPago(null, "Efectivo", null);
-            formaPago forma2 = new formaPago(null, "Tarjeta de credito", null);
-            formaPago forma3 = new formaPago(null, "Tarjeta Debito", null);
+            
+            FormaPago forma1 = new FormaPago(null, "Efectivo", null);
+            FormaPago forma2 = new FormaPago(null, "Tarjeta de credito", null);
+            FormaPago forma3 = new FormaPago(null, "Tarjeta Debito", null);
+            
             formaDePagoRepo.save(forma1);
             formaDePagoRepo.save(forma2);
             formaDePagoRepo.save(forma3);
+
+            
+            // Comprobante comp1 = new Comprobante(null, "Comp-001",null, 20000, "Tarjeta de Crédito", true, forma2, banco3);
+            // Comprobante comp2 = new Comprobante(null, "Comp-002",null, 10000, "Efectivo", true, forma1, banco2);
+            // Comprobante comp3 = new Comprobante(null, "Comp-003",null, 30000, "Tarjeta de Débito", true, forma3, banco1);
+            
+            Comprobante comp1 = new Comprobante();
+            comp1.setNumeroComprobante("COMP-001");
+            comp1.setMontoTotal(20000);
+            comp1.setMetodoPago("Tarjeta de Credito");
+            comp1.setEstadoPago(true);
+            comp1.setFormaPago(forma2);
+            comp1.setBanco(banco3);
+            comprobanteRepo.save(comp1);
+
+            Comprobante comp2 = new Comprobante();
+            comp2.setNumeroComprobante("COMP-002");
+            comp2.setMontoTotal(10000);
+            comp2.setMetodoPago("Efectivo");
+            comp2.setEstadoPago(true);
+            comp2.setFormaPago(forma1);
+            comp2.setBanco(banco2);
+            comprobanteRepo.save(comp2);
+
+            Comprobante comp3 = new Comprobante();
+            comp3.setNumeroComprobante("COMP-003");
+            comp3.setMontoTotal(30000);
+            comp3.setMetodoPago("Tarjeta Debito");
+            comp3.setEstadoPago(true);
+            comp3.setFormaPago(forma3);
+            comp3.setBanco(banco1);
+            comprobanteRepo.save(comp3);
+            
+
             System.out.println("Datos cargados con exito a la base de datos");
          }
 
--- MSFacturacion/src/main/java/cl/duoc/MSFacturacion/service/BancoService.java
+++ MSFacturacion/src/main/java/cl/duoc/MSFacturacion/service/BancoService.java
@@ -1,117 +1,191 @@
 package cl.duoc.MSFacturacion.service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import cl.duoc.MSFacturacion.model.banco;
-import cl.duoc.MSFacturacion.repository.bancoRepository;
+import cl.duoc.MSFacturacion.dto.ComprobanteDTO;
+import cl.duoc.MSFacturacion.model.Banco;
+import cl.duoc.MSFacturacion.repository.BancoRepository;
 import cl.duoc.MSFacturacion.repository.ComprobanteRepository;
-import cl.duoc.MSFacturacion.repository.FormaDePagoRepository;
+import cl.duoc.MSFacturacion.repository.FormaPagoRepository;
 import cl.duoc.MSFacturacion.model.Comprobante;
-import cl.duoc.MSFacturacion.model.formaPago;
+import cl.duoc.MSFacturacion.model.FormaPago;
 
 @Service
 public class BancoService {
 
     @Autowired
-    private bancoRepository bancoRepository;
+    private BancoRepository bancoRepository;
 
     @Autowired
     private ComprobanteRepository comprobanteRepository;
 
     @Autowired
-    private FormaDePagoRepository formaDePagoRepository;
+    private FormaPagoRepository formaPagoRepository;
 
-// listar bancos 
-    public List<banco> listarBancos() {
+//BANCO
+    public List<Banco> listarBancos() {
         return bancoRepository.findAll();
     }
-// guardar bancos 
-    public banco guardarBanco(banco nuevoBanco) {
-        if (bancoRepository.findByNombre(nuevoBanco.getNombreBanco()).isPresent()) {
-            throw new RuntimeException("Ya existe un banco con el nombre \"" + nuevoBanco.getNombreBanco() + "\".");
-        }
-        return bancoRepository.save(nuevoBanco);
+
+    public Banco buscarBancoPorId(Integer idBanco) {
+        return bancoRepository.findById(idBanco)
+            .orElseThrow(() -> new RuntimeException("Banco con id: " + idBanco + " no encontrado."));
     }
 
-// Buscar banco por id
-    public banco buscarBancoPorId(Integer id) {
-        return bancoRepository.findById(id)
-            .orElseThrow(() -> new RuntimeException("Banco con id " + id + " no encontrado."));
+    public Banco buscarBancoPorNombre(String nombreBanco) {
+        return bancoRepository.findByNombreBanco(nombreBanco)
+            .orElseThrow(() -> new RuntimeException("Banco con nombre: " + nombreBanco + " no encontrado."));
     }
-// Buscar Banco por nombre 
-    public banco buscarBancoPorNombre(String nombre) {
-        return bancoRepository.findByNombre(nombre)
-            .orElseThrow(() -> new RuntimeException("Banco con nombre \"" + nombre + "\" no encontrado."));
+
+    public Banco guardarBanco(Banco banco) {
+        if (bancoRepository.findByNombreBanco(banco.getNombreBanco()).isPresent()) {
+            throw new RuntimeException("Ya existe un banco con el nombre: " + banco.getNombreBanco() + ".");
+        }
+        return bancoRepository.save(banco);
     }
-// Actualizar bancos
-    public banco actualizarBanco(Integer id, banco bancoActualizado) {
-        banco bancoExistente = buscarBancoPorId(id);
+
+
+    public Banco actualizarBanco(Integer idBanco, Banco bancoActualizado) {
+        Banco bancoExistente = buscarBancoPorId(idBanco);
         bancoExistente.setNombreBanco(bancoActualizado.getNombreBanco());
         return bancoRepository.save(bancoExistente);
     }
-// eliminar banco por id 
-    public void eliminarBancoPorId(Integer id) {
-        buscarBancoPorId(id);
-        bancoRepository.deleteById(id);
+
+
+    public void eliminarBancoPorId(Integer idBanco) {
+        buscarBancoPorId(idBanco);
+        bancoRepository.deleteById(idBanco);
     }
-// Eliminar banco por nombre 
-    public void eliminarBancoPorNombre(String nombre) {
-        banco bancoExistente = buscarBancoPorNombre(nombre);
+
+
+    public void eliminarBancoPorNombre(String nombreBanco) {
+        Banco bancoExistente = buscarBancoPorNombre(nombreBanco);
         bancoRepository.deleteById(bancoExistente.getIdBanco());
 
     }
 
 
-// listar comprobantes por banco    
-    public List<Comprobante> listarComprobantesPorBanco(Integer bancoId) {
-        banco bancoExistente = buscarBancoPorId(bancoId);
-        return comprobanteRepository.findByBanco(bancoExistente);
+
+//COMPROBANTE
+
+    public List<Comprobante> listarTodosComprobantes(){
+        return comprobanteRepository.findAll();
+    }
+   
+    public List<Comprobante> listarComprobantesPorBanco(Integer idComprobante) {
+        Banco banco = buscarBancoPorId(idComprobante);
+        return comprobanteRepository.findByBanco(banco);
     }
 
-// listar formas de pago por banco
-    public List<formaPago> listarFormasDePagoPorBanco(Integer bancoId) {
-        banco bancoExistente = buscarBancoPorId(bancoId);
-        List<Comprobante> comprobantes = comprobanteRepository.findByBanco(bancoExistente);
+
+    public Comprobante generarComprobante(Comprobante comprobante) {
+        if(comprobanteRepository.findByNumeroComprobante(comprobante.getNumeroComprobante()).isPresent()){
+            throw new RuntimeException("Ya existe un comprobante con número: " + comprobante.getNumeroComprobante());
+        }
+        buscarBancoPorId(comprobante.getBanco().getIdBanco());
+
+        return comprobanteRepository.save(comprobante);
+    }
+    
+
+    public Comprobante buscarComprobantePorNumero(String numeroComprobante) {
+        return comprobanteRepository.findByNumeroComprobante(numeroComprobante)
+                .orElseThrow(() -> new RuntimeException("Comprobante con número: " + numeroComprobante + " no encontrado."));
+    }
+
+    // anular comprobante por numero de comprobante dejando el numero de comprobante con el estado anulado
+    public Comprobante anularComprobantePorNumero(String numeroComprobante) {
+        Comprobante comprobante = buscarComprobantePorNumero(numeroComprobante);
+        comprobante.setEstadoPago(false);
+        return comprobanteRepository.save(comprobante);
+    }
+
+    // filtrar comprobantes por fecha de emision
+    public List<Comprobante> filtrarComprobantesPorFechaEmision(Integer idBanco, LocalDateTime fechaEmision) {
+        Banco banco = buscarBancoPorId(idBanco);
+        return comprobanteRepository.findByBancoAndFechaEmision(banco, fechaEmision);
+                
+    }
+
+
+//FORMA DE PAGO
+
+    public List<FormaPago> listarTodasFormasPago(){
+        return formaPagoRepository.findAll();
+    }
+
+    public List<FormaPago> listarFormasDePagoPorIdBanco(Integer idBanco) {
+        Banco banco = buscarBancoPorId(idBanco);
+        List<Comprobante> comprobantes = comprobanteRepository.findByBanco(banco);
         return comprobantes.stream()
                 .map(Comprobante::getFormaPago)
                 .distinct()
                 .toList();
     }
-// generar comprobantes
-    public Comprobante generarComprobante(Integer bancoId, String numeroComprobante, String medioDePago) {
-        banco bancoExistente = buscarBancoPorId(bancoId);
-        formaPago formaPagoExistente = formaDePagoRepository.findByNombre(medioDePago)
-                .orElseThrow(() -> new RuntimeException("Forma de pago \"" + medioDePago + "\" no encontrada."));
 
-        Comprobante nuevoComprobante = new Comprobante();
-        nuevoComprobante.setNumeroComprobante(numeroComprobante);
-        nuevoComprobante.setBanco(bancoExistente);
-        nuevoComprobante.setFormaPago(formaPagoExistente);
 
-        return comprobanteRepository.save(nuevoComprobante);
-    }
-    //buscar comprobante por numero de comprobante
-    public Comprobante buscarComprobantePorNumero(String numeroComprobante) {
-        return comprobanteRepository.findByNumeroComprobante(numeroComprobante)
-                .orElseThrow(() -> new RuntimeException("Comprobante con número \"" + numeroComprobante + "\" no encontrado."));
-    }
-    // anular comprobante por numero de comprobante dejando el numero de comprobante con el estado anulado
-    public Comprobante anularComprobantePorNumero(String numeroComprobante) {
-        Comprobante comprobanteExistente = buscarComprobantePorNumero(numeroComprobante);
-        comprobanteExistente.setEstadopago(false);
-        return comprobanteRepository.save(comprobanteExistente);
-    }
-// filtrar comprobantes por fecha de emision
-    public List<Comprobante> filtrarComprobantesPorFechaEmision(Integer bancoId, String fechaEmision) {
-        banco bancoExistente = buscarBancoPorId(bancoId);
-        List<Comprobante> comprobantes = comprobanteRepository.findByBanco(bancoExistente);
+    public List<FormaPago> listarFormasDePagoPorNombreBanco(String nombreBanco) {
+        Banco banco = buscarBancoPorNombre(nombreBanco);
+        List<Comprobante> comprobantes = comprobanteRepository.findByBanco(banco);
         return comprobantes.stream()
-                .filter(c -> c.getFechaEmision().toString().equals(fechaEmision))
+                .map(Comprobante::getFormaPago)
+                .distinct()
                 .toList();
     }
+
+
+
+    public FormaPago buscarFormaPagoPorSuId(Integer idFormaPago){
+        return formaPagoRepository.findById(idFormaPago)
+            .orElseThrow(() -> new RuntimeException("Forma de pago con id: " + idFormaPago + " no encontrada"));
+    }
+
+    public FormaPago buscarFormaPagoPorMedio(String medioDePago) {
+        return formaPagoRepository.findByMedioDePago(medioDePago)
+            .orElseThrow(() -> new RuntimeException("Forma de pago: " + medioDePago + " no encontrada."));
+    }
+
+
+
+    public FormaPago guardarFormaPago(FormaPago formaPago) {
+        if (formaPagoRepository.findByMedioDePago(formaPago.getMedioDePago()).isPresent()) {
+            throw new RuntimeException("Ya existe la forma de pago: " + formaPago.getMedioDePago());
+        }
+        return formaPagoRepository.save(formaPago);
+    }
+
+    public FormaPago actualizarFormaPago(Integer idFormaPago, FormaPago formaPagoActualizado) {
+        FormaPago formaPago = buscarFormaPagoPorSuId(idFormaPago);
+        formaPago.setMedioDePago(formaPagoActualizado.getMedioDePago());
+        return formaPagoRepository.save(formaPago);
+    }
+
+    public void eliminarFormaPago(Integer idFormaPago) {
+        buscarFormaPagoPorSuId(idFormaPago);
+        formaPagoRepository.deleteById(idFormaPago);
+    }
+
+
+
+    //COMUNICACION CON OTRO MS
+    public ComprobanteDTO buscarComprobanteDTOPorId(Integer idComprobante) {
+        Comprobante comprobante = comprobanteRepository.findById(idComprobante)
+            .orElseThrow(() -> new RuntimeException("Comprobante con id: " + idComprobante + " no encontrado."));
+
+        ComprobanteDTO dto = new ComprobanteDTO();
+        dto.setIdComprobante(comprobante.getIdComprobante());
+        dto.setNumeroComprobante(comprobante.getNumeroComprobante());
+        dto.setMontoTotal(comprobante.getMontoTotal());
+        dto.setEstadoPago(comprobante.isEstadoPago());
+
+        return dto;
+    }
+
+
 }
 
         
