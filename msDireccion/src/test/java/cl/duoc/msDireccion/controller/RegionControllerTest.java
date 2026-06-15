4d992a3f627c231c49b35da83ac9910274d2a80f
cote-huaiqui <ma.crucesh@duocuc.cl>
2026-06-15 11:57:31 -0400

    prueba ms direccion

--- msDireccion/src/test/java/cl/duoc/msDireccion/controller/RegionControllerTest.java
+++ msDireccion/src/test/java/cl/duoc/msDireccion/controller/RegionControllerTest.java
@@ -3,6 +3,8 @@ package cl.duoc.msDireccion.controller;
 
 
 import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -11,6 +13,8 @@ import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
+
+import cl.duoc.msDireccion.dto.RegionDTO;
 import cl.duoc.msDireccion.model.Region;
 import cl.duoc.msDireccion.service.RegionService;
 
@@ -23,14 +27,15 @@ public class RegionControllerTest {
     @MockitoBean
     private RegionService regionService; //service falso
 
-    private Region regionEjemplo;
+    private RegionDTO regionEjemplo;
 
     @BeforeEach
     void setUp(){
-        regionEjemplo = new Region();
+        regionEjemplo = new RegionDTO();
         regionEjemplo.setIdRegion(1);
         regionEjemplo.setNombreRegion("Metropolitana");
 
+        
 
     }
 
@@ -41,24 +46,25 @@ public class RegionControllerTest {
 
 
     @Test
-    void buscarPorId_retorna200(){
+    void buscarPorId_retorna200() throws Exception{
         //ARRANGE: el service debe retornar una region 
         when(regionService.buscarRegionPorId(1)).thenReturn(regionEjemplo);
 
         //ACT + ASSERT
-        mock.perform(get("/api/v1/regiones/1")).andExpect(status().isOk());  //el mock simula las llamadas http, puede hacer delete, post, get, etc
+        mock.perform(get("/api/v1/direccion/buscar/region-resumida/id/1"))
+            .andExpect(status().isOk());  //el mock simula las llamadas http, puede hacer delete, post, get, etc
 
 
     }
 
 
     @Test 
-    void buscarPorId_retorna404(){
+    void buscarPorId_retorna404() throws Exception{
         //ARRANGE: buscamos unz region con id 99 y tira un error
-        when(regionService.buscarRegionPorId(99)).thenThrow(new RuntimeException("Region no encontrada"));
+        when(regionService.buscarRegionPorId(99)).thenThrow(new RuntimeException("Region con id 99 no encontrada"));
 
         //ACT + ASSERT
-        mock.perform(get("/api/v1/regiones/99")).andExpect(status().isNotFound());  // o sea un codigo HTTPS 404
+        mock.perform(get("/api/v1/direccion/buscar/region-resumida/id/99")).andExpect(status().isNotFound());  // o sea un codigo HTTPS 404
 
 
 
